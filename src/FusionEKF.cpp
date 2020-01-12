d4e44b6f182735af6036bf4c9d63adeec63f85ab
timothylimyl <timothylimyonglee@gmail.com>
2020-01-12 04:35:22 +0000

    Implemented EKF and push to remote repo

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,8 +36,25 @@ FusionEKF::FusionEKF() {
    * TODO: Finish initializing the FusionEKF.
    * TODO: Set the process and measurement noises
    */
+  
+  //measurement matrix - laser
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
+  
 
+  
+  //KF Variables
+  ekf_.P_ = MatrixXd(4, 4);
+  ekf_.P_ << 1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1000, 0,
+             0, 0, 0, 1000;
 
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
 }
 
 /**
@@ -50,64 +67,95 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    * Initialization
    */
   if (!is_initialized_) {
-    /**
-     * TODO: Initialize the state ekf_.x_ with the first measurement.
-     * TODO: Create the covariance matrix.
-     * You'll need to convert radar from polar to cartesian coordinates.
-     */
+
 
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
+    
+    
 
-    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-      // TODO: Convert radar from polar to cartesian coordinates 
-      //         and initialize state.
+
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR ) {
+      
+      double rho = measurement_pack.raw_measurements_(0);
+      double phi = measurement_pack.raw_measurements_(1);
+      double rhodot = measurement_pack.raw_measurements_(2);
+      
+      // rho = distance (hypotenuse)  , phi  = angle , rhodot = velocity (hyp)
+      ekf_.x_ << rho * cos(phi), rho * sin(phi), rhodot * cos(phi), rhodot * sin(phi);
 
     }
-    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      // TODO: Initialize state.
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER ) {
+      
+      // Transition matrix (H) will remove the velocity when comparing to measurements
+      ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0; 
+      // x, y, vx, vy 
 
     }
+    
+    previous_timestamp_ = measurement_pack.timestamp_;
+
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
   }
+  
+  // ******** PREDICTION **************************************** //
+  bool on_Radar = false;
+  bool on_Lidar = false;
+  //******************* use boolean logic to switch radar/lidar on and off.
+  
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0; //in second (time change)
+  previous_timestamp_ = measurement_pack.timestamp_; // store current time to use for next time step
+  
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+  
+  // In F matrix, to get distance (it will multiply with constant velocity):
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+  
+  // set measurement noises
+  float noise_ax = 9;
+  float noise_ay = 9;
+  
+  //set the process covariance matrix Q
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ << dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+            0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+            dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+            0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
 
-  /**
-   * Prediction
-   */
 
-  /**
-   * TODO: Update the state transition matrix F according to the new elapsed time.
-   * Time is measured in seconds.
-   * TODO: Update the process noise covariance matrix.
-   * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
-   */
 
-  ekf_.Predict();
+  ekf_.Predict(); // Predict the state 
+  
+//***********************************************************
 
-  /**
-   * Update
-   */
-
-  /**
-   * TODO:
-   * - Use the sensor type to perform the update step.
-   * - Update the state and covariance matrices.
-   */
-
-  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-    // TODO: Radar updates
-
-  } else {
-    // TODO: Laser updates
+// ******************  UPDATE *******************************
+  
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR && on_Radar == 1) {
+    
+   
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+ 
+  } 
+    if (measurement_pack.sensor_type_ == MeasurementPackage::LASER && on_Lidar == 1) {
+    // LIDAR
+    
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
 
   }
 
   // print the output
-  cout << "x_ = " << ekf_.x_ << endl;
-  cout << "P_ = " << ekf_.P_ << endl;
+  //cout << "x_ = " << ekf_.x_ << endl;
+ // cout << "P_ = " << ekf_.P_ << endl;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -1,9 +1,13 @@
 #include "tools.h"
 #include <iostream>
 
+
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
 
 Tools::Tools() {}
 
@@ -11,14 +15,63 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+  
+    VectorXd error_current(estimations[0].size());  // same size vector 
+    VectorXd error_sum(estimations[0].size());  // same size vector 
+    VectorXd rmse(estimations[0].size());  // same size vector 
+  
+  	// Exception checking, ensure same size:
+    if (estimations.size() != ground_truth.size()
+      || estimations.size() == 0 || estimations[0].size() != ground_truth[0].size() )     {
+    cout << "Invalid estimation or ground_truth data" << endl;
+    return rmse;
+    }
+
+
+    for (unsigned int i = 0; i < estimations.size(); ++i) {
+
+        error_current = (estimations[i] - ground_truth[i]).array().pow(2);
+        if (i == 0) {
+            error_sum = error_current;
+        }
+        else {
+            error_sum = error_sum + error_current;
+        }
+    }
+    rmse  = (error_sum / estimations.size()).array().sqrt();
+  
+  	return rmse;
+  
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+  
+  MatrixXd Hj(3,4);
+    
+  //recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+    
+  // Some re-used equations for matrix below
+  float c1 = px * px + py * py;
+  float c2 = sqrt(c1);
+  float c3 = c1 * c2;
+  
+  //check division by zero
+  if(fabs(c1) < 0.0001){
+    std::cout << "Function CalculateJacobian() has Error: Division by Zero" << std::endl;
+    return Hj; // out of function
+  }
+    
+  //compute the Jacobian matrix
+  Hj << (px/c2), (py/c2), 0, 0,
+        -(py/c1), (px/c1), 0, 0,
+        (py*(vx*py - vy*px))/c3, (px*(vy*px - vx*py))/c3, px/c2, py/c2;
+    
+  //return the matrix
+  return Hj;
+  
+  
 }
